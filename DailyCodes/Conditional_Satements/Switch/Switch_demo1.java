33e13f3fff047c2130b43b7f708bb9973d5f4082
Prathmesh07778 <prathmesh07.github@gmail.com>
2026-03-24 23:58:11 +0530

    assignments

---
+++ DailyCodes/Conditional_Satements/Switch/Switch_demo1.java
@@ -0,0 +1,22 @@
+class Switch_demo1 {
+
+    public static void main(String args[]
+
+        int x = 3;
+
+        switch(x) {
+
+            Case 1: 
+                    System.out.println("One");
+                    break;
+            
+            Case 2:
+                    System.out.println("Two");
+                    break;
+            
+            default:
+                 System.out.println("Error");
+            
+        }
+    )
+}
